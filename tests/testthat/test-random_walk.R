987a71719f5b76cf0c84fea197baec4c649373bc
mlhollestelle <martijnhollestelle@gmail.com>
2021-12-31 17:58:42 +0200

    avoid collision random walk
    in addition, refactoring

--- tests/testthat/test-random_walk.R
+++ tests/testthat/test-random_walk.R
@@ -1,11 +1,6 @@
-steps = 4
-walk = generate_steps(steps)
+segments = 4
+walk = generate_walk(n_segments = segments)
 
-test_that("generate_steps() works", {
-  expect_equal(dim(walk), c(steps, 3))
-})
-
-test_that("random_walk_coords() works", {
-  points = random_walk_coords(walk)
-  expect_equal(dim(points), c(steps + 1, 2))
+test_that("generate_walk() works", {
+  expect_equal(dim(walk), c(segments, 5))
 })
