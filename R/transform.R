51c37c9b389f036b60b5519a2fd5ee6294115198
mlhollestelle <martijnhollestelle@gmail.com>
2022-01-01 12:02:55 +0200

    Adjust projection

--- R/transform.R
+++ R/transform.R
@@ -1,5 +1,5 @@
-.cabinet_projection = function(walk, alpha = 63.4) {
+.cabinet_projection = function(walk, alpha = 45) {
   walk$x = walk$x + 0.5 * walk$y * cos(alpha)
-  walk$y = walk$y + 0.5 * walk$y * sin(alpha)
+  walk$y = 0.5 * walk$y
   return(walk)
 }
