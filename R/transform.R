b93e3b22f5407416fbab5ecc6b8d4d762be81e22
mlhollestelle <martijnhollestelle@gmail.com>
2022-01-01 13:23:32 +0200

    add extrusion of path

--- R/transform.R
+++ R/transform.R
@@ -3,3 +3,27 @@
   walk$y = 0.5 * walk$y
   return(walk)
 }
+
+#' Extrude a random walk.
+#'
+#' @param walk a random walk.
+#' @param z extrusion value.
+#'
+#' @return a data frame.
+#' @export
+.extrude_walk = function(walk, z = 1) {
+  rep_rows = c(1, rep(seq_len(nrow(walk)-1)[-1], each = 2), nrow(walk))
+  walk_grouped = walk[rep_rows, c("x", "y")]
+  walk_grouped$group = rep(seq_len(nrow(walk_grouped)/2), each = 2)
+  z = 1
+  walk_extruded = plyr::ddply(walk_grouped, .variables = "group", function(element) {
+    element_extruded = element
+    element_extruded$y = element_extruded$y + z
+    polygon = rbind(
+      element[],
+      element_extruded[rev(seq_len(nrow(element_extruded))), ]
+    )
+    return(polygon)
+  })
+  return(walk_extruded)
+}
