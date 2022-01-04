f90177434fe91bac12fdbfe37cf5a279775a5166
mlhollestelle <martijnhollestelle@gmail.com>
2022-01-04 20:06:43 +0200

    align background and add spacing around walk

--- R/background.R
+++ R/background.R
@@ -1,11 +1,29 @@
 #' Generate a background pattern.
 #'
 #' @param res resolution of the pattern.
+#' @param walk a walk.
+#' @param border spacing around walk.
 #'
 #' @return a data frame of points.
 #' @export
-generate_background_pattern = function(res = 0.05) {
-  pattern = expand.grid(x = seq(-10, 10, res),
-                        y = seq(-10, 10, res))
+generate_background_pattern = function(walk, res = 0.05, border = 4) {
+  bbox = get_bbox(walk)
+  x = seq(bbox$x[1] - border, bbox$x[2] + border, by = res)
+  y = seq(bbox$y[1] - border, bbox$y[2] + border, by = res)
+  pattern = expand.grid(x = x, y = y)
   return(pattern)
+}
+
+#' Get bounding box of an object.
+#'
+#' @param df a data frame with an x and y column.
+#'
+#' @return a data frame.
+#' @export
+get_bbox = function(df) {
+  bbox = data.frame(
+    x = range(walk$x),
+    y = range(walk$y)
+  )
+  return(bbox)
 }
