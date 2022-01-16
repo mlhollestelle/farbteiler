d1fb167cca594d879fd79a42ed71d58414f4ae64
mlhollestelle <martijnhollestelle@gmail.com>
2022-01-16 19:05:13 +0200

    minor tweaks

--- R/transform.R
+++ R/transform.R
@@ -18,7 +18,6 @@
   rep_rows = c(1, rep(seq_len(nrow(walk)-1)[-1], each = 2), nrow(walk))
   walk_grouped = walk[rep_rows, c("x", "y")]
   walk_grouped$group = rep(seq_len(nrow(walk_grouped)/2), each = 2)
-  z = 1
   walk_extruded = plyr::ddply(walk_grouped,
                               .variables = "group",
                               function(element) {
